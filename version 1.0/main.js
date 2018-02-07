725f5e2a48d5b935d3fd2f432f1db7c968200efb
William Cheng <willchengsters@gmail.com>
2018-02-07 13:07:00 -0800

    able to create cards after removing all .flip divs

--- version 1.0/main.js
+++ version 1.0/main.js
@@ -4,7 +4,9 @@ $(document).ready(initiateApp);
 
 function initiateApp(){
     createCards();
-    $('.outer').on('click', flip);
+    // $('.flip').on('click', flip);
+    $('#game-area').on('click', '.flip' , flip );
+    $('.game-area').on('click', '.flip', card_clicked);
 }
 
 function flip(){
@@ -23,40 +25,45 @@ var cardFrontArray = [
     'url(../images/your-name.jpg)'
 ];
 
+var cards=[];
+
 function createCards(){
+    var newCardsArray = cardFrontArray.slice();
 
     for(var i=0; i<9; i++){
-        for (var x=0; x<2; x++){
-            var outerCard = $('<div>').addClass('outer');
-            var card = $('<div>').addClass('card');
-            var front = $('<div>').addClass('front');
-            var back = $('<div>').addClass('back');
-            var frontIndex = Math.floor(Math.random()*9);
+        var cardflip = $('<div>').addClass('flip');
+        var card = $('<div>').addClass('card');
+        var front = $('<div>').addClass('front');
+        var back = $('<div>').addClass('back');
+        var frontIndex = Math.floor(Math.random()*(newCardsArray.length));
+        var gameArea = $('#game-area');
 
-            $(front).css(
-                'background-image', cardFrontArray[frontIndex]
-            );
+        $(front).css(
+            'background-image', newCardsArray[frontIndex]
+        );
 
-            $(back).css(
-                'background-image', 'url(../images/yugioh-card-back.jpg)'
-            );
-            $(card).append(front);
-            $(card).append(back);
-            $(outerCard).append(card);
-            $('#game-area').append(outerCard);
+        newCardsArray.splice(frontIndex,1);
 
-        }
+        $(back).css(
+            'background-image', 'url(../images/yugioh-card-back.jpg)'
+        );
+        $(card).append(front);
+        $(card).append(back);
+        $(cardflip).append(card);
+        // $(gameArea).append(cardflip);
+        var cardflip2 = $(cardflip).clone();
+        // $(gameArea).append(card2);
+        cards.push(cardflip);
+        cards.push(cardflip2);
 
 
     }
-
-
-
-
-
-
-
-
-
+    console.log(newCardsArray);
+    for(var i=0; i<18; i++){
+        var randomIndex = Math.floor(Math.random()*(cards.length));
+        $(gameArea).append(cards[randomIndex]);
+        cards.splice(randomIndex,1)
+    }
 
 }
+
